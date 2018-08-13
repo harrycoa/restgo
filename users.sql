7ab1de0d5164901c042f8b2be947141cf49a67c3
harry <rodol_2000@hotmail.com>
2018-08-13 11:45:53 -0500

    ultimas configuraciones

--- users.sql
+++
@@ -1,7 +0,0 @@
-create database users(
-  id int(6) unsiged auto_increment primary key,
-  username varchar(100) not null,
-  password varchar(256) not null,
-  email varchar(200) null,
-  create_date timestamp default current_timestamp
-);
